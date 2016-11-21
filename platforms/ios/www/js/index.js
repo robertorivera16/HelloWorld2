ce50cf995852402ce8cd48c688ed2e2b5c307ddf
Roberto Rivera Reyes <roberto.rivera16@upr.edu>
2016-11-21 16:05:56 -0400

    Hello World Page 50% completed

---
+++ platforms/ios/www/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#helloworld-text").css({
+          "text-align":"center",
+          "color": "white",
+          "font-size": "250%"});
+    });
+});
